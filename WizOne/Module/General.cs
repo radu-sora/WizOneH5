95275c477fc7cb980eca7df82e712bb8efc908f5
Florin Ciurlau <florin.ciurlau@Wizrom.ro>
2019-10-01 14:28:30 +0300

    - corectii integrare;

--- WizOne/Module/General.cs
+++ WizOne/Module/General.cs
@@ -8376,14 +8376,24 @@ namespace WizOne.Module
             }
         }
 
-        public static string FiltrulGol(string camp)
+        public static string FiltrulCuNull(string camp)
         {
             string str = "";
 
             try
             {
-                if (Constante.tipBD == 1 && camp != "")
-                    str = $@" AND ""{camp}"" <> '' "; 
+                if (camp != "")
+                {
+                    if (camp.IndexOf(".") >= 0)
+                        camp = camp.Replace(".", ".\"");
+                    else
+                        camp = "\"" + camp;
+
+                    camp = camp + "\"";
+                    str = " AND " + camp + " IS NOT NULL ";
+                    if (Constante.tipBD == 1)
+                        str += $@" AND {camp} <> '' ";
+                }
             }
             catch (Exception ex)
             {

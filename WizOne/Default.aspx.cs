994e84d5bc215f5d5b554a277a8aa30502ad8f8c
radu.sora <radu.sora@WizromSoftwareSRL.onmicrosoft.com>
2020-05-20 11:36:55 +0300

    Tactil (pagina extra pt. param = 3)

--- WizOne/Default.aspx.cs
+++ WizOne/Default.aspx.cs
@@ -71,10 +71,12 @@ namespace WizOne
                 {
                     //Radu 23.04.2020
                     string tip = Dami.ValoareParam("TipInfoChiosc", "0");
-                    if (tip == "0" || tip == "3")
+                    if (tip == "0")
                         Response.Redirect("DefaultTactil.aspx", false);
-                    else
+                    else if (tip == "1" || tip == "2")
                         Response.Redirect("DefaultTactilFaraCard.aspx", false);
+                    else if (tip == "3")
+                        Response.Redirect("DefaultTactilExtra.aspx", false);
                     return;
                 }
 
--- WizOne/DefaultTactil.aspx.designer.cs
+++ WizOne/DefaultTactil.aspx.designer.cs
@@ -7,13 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WizOne
-{
-
-
-    public partial class DefaultTactil
-    {
-
+namespace WizOne {
+    
+    
+    public partial class DefaultTactil {
+        
         /// <summary>
         /// form1 control.
         /// </summary>
@@ -22,25 +20,7 @@ namespace WizOne
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// panouExt control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl panouExt;
-
-        /// <summary>
-        /// butoane control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl butoane;
-
+        
         /// <summary>
         /// txtPan1 control.
         /// </summary>
@@ -49,113 +29,5 @@ namespace WizOne
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlInputText txtPan1;
-
-        /// <summary>
-        /// btn7 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn7;
-
-        /// <summary>
-        /// btn8 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn8;
-
-        /// <summary>
-        /// btn9 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn9;
-
-        /// <summary>
-        /// btn4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn4;
-
-        /// <summary>
-        /// btn5 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn5;
-
-        /// <summary>
-        /// btn6 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn6;
-
-        /// <summary>
-        /// btn1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn1;
-
-        /// <summary>
-        /// btn2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn2;
-
-        /// <summary>
-        /// btn3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn3;
-
-        /// <summary>
-        /// btn0 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btn0;
-
-        /// <summary>
-        /// btnLog control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxButton btnLog;
-
-        /// <summary>
-        /// hfPIN control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxHiddenField hfPIN;
     }
 }

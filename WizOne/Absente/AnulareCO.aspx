686c07daf5542973537fc3af4f22df530635a9e0
radu.sora <radu.sora@WizromSoftwareSRL.onmicrosoft.com>
2020-06-10 14:11:36 +0300

    AnulareCO (dezv)

---
+++ WizOne/Absente/AnulareCO.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Cadru.Master" AutoEventWireup="true" CodeBehind="AnulareCO.aspx.cs" Inherits="WizOne.Absente.AnulareCO" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <script language="javascript" type="text/javascript">
+        
+
+        function OnEndCallback(s, e) {
+            pnlLoading.Hide();
+            if (s.cpAlertMessage != null) {
+                swal({
+                    title: "", text: s.cpAlertMessage,
+                    type: "warning"
+                });
+                s.cpAlertMessage = null;
+            }
+        }
+
+
+
+    </script>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <table width="100%">
+        <tr>
+            <td align="left">
+                <dx:ASPxLabel ID="txtTitlu" runat="server" Text="" Font-Size="14px" Font-Bold="true" ForeColor="#00578a" Font-Underline="true" />
+            </td>
+            <td align="right">
+                <dx:ASPxButton ID="btnAnulare" ClientInstanceName="btnAnulare" ClientIDMode="Static" runat="server" Text="Anulare" AutoPostBack="false" oncontextMenu="ctx(this,event)" >
+                    <ClientSideEvents Click="function(s, e) {
+                        pnlLoading.Show();
+                        pnlCtl.PerformCallback('btnAnulare');
+                    }" />
+                    <Image Url="~/Fisiere/Imagini/Icoane/sterge.png"></Image>
+                </dx:ASPxButton>  
+                <dx:ASPxButton ID="btnExit" ClientInstanceName="btnExit" ClientIDMode="Static" runat="server" Text="Iesire" AutoPostBack="true" PostBackUrl="../Pagini/MainPage.aspx" oncontextMenu="ctx(this,event)" >
+                    <Image Url="~/Fisiere/Imagini/Icoane/iesire.png"></Image>
+                </dx:ASPxButton>
+            </td>
+        </tr>
+    </table>
+    
+
+    <dx:ASPxCallbackPanel ID="pnlCtl" ClientIDMode="Static" ClientInstanceName="pnlCtl" runat="server" OnCallback="pnlCtl_Callback" SettingsLoadingPanel-Enabled="false" >
+        <ClientSideEvents EndCallback="function (s,e) { OnEndCallback(s,e); }" CallbackError="function (s,e) { pnlLoading.Hide(); }" />
+        <PanelCollection>
+            <dx:PanelContent>
+            <table style="width:55%;">
+                <tr>
+                    <td style="padding-right:15px !important;">
+                        <label id="lblAnLuna" runat="server" oncontextMenu="ctx(this,event)">Luna/An</label><br />
+                        <dx:ASPxDateEdit ID="txtAnLuna" ClientInstanceName="txtAnLuna" ClientIDMode="Static" runat="server" Width="100px" DisplayFormatString="MM/yyyy" PickerType="Months" EditFormatString="MM/yyyy" EditFormat="Custom" oncontextMenu="ctx(this,event)" >     
+                            <ClientSideEvents ValueChanged="function(s, e) { pnlCtl.PerformCallback('txtAnLuna'); }" />
+                            <CalendarProperties FirstDayOfWeek="Monday" />
+                        </dx:ASPxDateEdit>
+                    </td>
+                    <td style="padding-right:15px !important;">
+                        <dx:ASPxLabel  ID="lblPer" runat="server"  style="display:inline-block;"  Text="Perioada"></dx:ASPxLabel>
+                        <dx:ASPxComboBox ID="cmbPerioada" runat="server" ClientInstanceName="cmbPerioada" ClientIDMode="Static" Width="100px" ValueField="Id" DropDownWidth="100" 
+                            TextField="Denumire" ValueType="System.Int32" AutoPostBack="false">
+                        </dx:ASPxComboBox>
+                    </td>                                      	
+                </tr>
+            </table>
+            <br />
+            <table style="width:100%;">
+                <tr>
+                    <td >
+                        <dx:ASPxGridView ID="grDate" runat="server" ClientInstanceName="grDate" ClientIDMode="Static" Width="100%" AutoGenerateColumns="false"  >
+                            <SettingsBehavior AllowFocusedRow="true"  />
+                            <Settings ShowFilterRow="true" ShowColumnHeaders="true" />  
+                            <SettingsEditing Mode="Inline" />  
+                            <ClientSideEvents  ContextMenu="ctx" EndCallback="function(s,e) { OnEndCallback(s,e); }" />           
+                            <Columns>  
+                                <dx:GridViewCommandColumn Width="30px" VisibleIndex="0" ButtonType="Image" Caption=" " ShowSelectCheckbox="true" FixedStyle="Left" SelectAllCheckboxMode="AllPages" />
+                                 <dx:GridViewDataComboBoxColumn FieldName="F10003" Name="F10003" Caption="Angajat" ReadOnly="true" >           
+                                     <Settings SortMode="DisplayText" />
+                                    <PropertiesComboBox TextField="NumeComplet" ValueField="F10003" ValueType="System.Int32" DropDownStyle="DropDown">                                        
+                                    </PropertiesComboBox>
+                                </dx:GridViewDataComboBoxColumn>        
+						        <dx:GridViewDataTextColumn FieldName="Stare" Name="Stare" Caption="Stare" ReadOnly="true" Width="100px"  />
+                                <dx:GridViewDataTextColumn FieldName="ZileCO" Name="ZileCO" Caption="Zile CO (total)" ReadOnly="true" Width="100px"  />
+                                <dx:GridViewDataTextColumn FieldName="ZileCOAnC" Name="ZileCOAnC" Caption="Zile CO (an curent)" ReadOnly="true" Width="100px"  />
+                                <dx:GridViewDataTextColumn FieldName="ZileCOAnAnt" Name="ZileCOAnAnt" Caption="Zile CO (an anterior)" ReadOnly="true" Width="100px"  />
+                                <dx:GridViewDataTextColumn FieldName="ZileCOAnAnt2" Name="ZileCOAnAnt2" Caption="Zile CO (an anterior 2)" ReadOnly="true" Width="100px"  />
+                                <dx:GridViewDataTextColumn FieldName="ZileCOMaiVechi" Name="ZileCOMaiVechi" Caption="Zile CO (mai vechi)" ReadOnly="true" Width="100px"  />                    
+                                <dx:GridViewDataTextColumn FieldName="IdAuto" Name="IdAuto" Caption="IdAuto" Visible="false" ReadOnly="true" Width="100px"  />	
+                            </Columns>
+                        </dx:ASPxGridView>
+                    </td>
+                </tr>
+            </table>
+
+          </dx:PanelContent>
+        </PanelCollection>
+    </dx:ASPxCallbackPanel>
+
+
+    
+
+</asp:Content>
---
+++ WizOne/Absente/AnulareCO.aspx.cs
@@ -0,0 +1,187 @@
+﻿using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using WizOne.Module;
+using System.Data.OleDb;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WizOne.Absente
+{
+    public partial class AnulareCO : System.Web.UI.Page
+    {
+        //int F10003 = -99;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Dami.AccesApp();
+
+                #region Traducere
+                string ctlPost = Request.Params["__EVENTTARGET"];
+                //if (!string.IsNullOrEmpty(ctlPost) && ctlPost.IndexOf("LangSelectorPopup") >= 0) Constante.IdLimba = ctlPost.Substring(ctlPost.LastIndexOf("$") + 1).Replace("a", "");
+
+                #endregion
+                
+                btnExit.Text = Dami.TraduCuvant("btnExit", "Iesire");
+                btnAnulare.Text = Dami.TraduCuvant("btnAnulare", "Anulare");
+
+                if (!IsPostBack)
+                    txtAnLuna.Value = DateTime.Now;
+
+                DataTable dtPer = new DataTable();
+                dtPer.Columns.Add("Id", typeof(int));
+                dtPer.Columns.Add("Denumire", typeof(string));
+
+                dtPer.Rows.Add(1, "Pana in " + (Convert.ToDateTime(txtAnLuna.Value).Year - 1).ToString() + " inclusiv");
+                dtPer.Rows.Add(2, "Pana in " + (Convert.ToDateTime(txtAnLuna.Value).Year - 2).ToString() + " inclusiv");
+                dtPer.Rows.Add(3, "Mai vechi");
+
+                cmbPerioada.DataSource = dtPer;
+                cmbPerioada.DataBind();
+                if (IsPostBack)
+                {
+                    DataTable dt = Session["AnulareCO_Grid"] as DataTable;
+                    grDate.KeyFieldName = "IdAuto";
+                    grDate.DataSource = dt;
+                    grDate.DataBind();
+                }
+                else
+                {
+                    IncarcaGrid();
+                }
+
+                DataTable dtAng = General.IncarcaDT(Dami.SelectAngajati(), null);
+                GridViewDataComboBoxColumn colAng = (grDate.Columns["F10003"] as GridViewDataComboBoxColumn);
+                colAng.PropertiesComboBox.DataSource = dtAng;        
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, Path.GetFileName(Page.AppRelativeVirtualPath), new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+        internal static string SelectAngajati()
+        {
+            string strSql = "";
+
+            try
+            {
+                strSql = $@"SELECT A.F10003, A.F10008 {Dami.Operator()} ' ' {Dami.Operator()} A.F10009 AS NumeComplet, G.F00406 AS Filiala, H.F00507 AS Sectie, I.F00608 AS Departament
+                        FROM F100 A
+                        LEFT JOIN F004 G ON A.F10005 = G.F00405
+                        LEFT JOIN F005 H ON A.F10006 = H.F00506
+                        LEFT JOIN F006 I ON A.F10007 = I.F00607";
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, "Dami", new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+
+            return strSql;
+        }
+
+
+        protected void pnlCtl_Callback(object source, CallbackEventArgsBase e)
+        {
+            try
+            {
+                string ctrl = e.Parameter.Split(';')[0];
+                switch (ctrl)
+                {
+                    case "txtAnLuna":
+                        IncarcaGrid();
+                        break;
+                    case "btnAnulare":
+                        Anulare();
+                        break;
+                } 
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, Path.GetFileName(Page.AppRelativeVirtualPath), new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+
+
+        private void IncarcaGrid()
+        {
+            try
+            {
+
+                if (txtAnLuna.Value == null)
+                {
+                    pnlCtl.JSProperties["cpAlertMessage"] = Dami.TraduCuvant("Nu ati selectat luna si anul!");
+                    return;
+                }
+
+                if (IsPostBack)
+                {
+                    DataTable dtPer = new DataTable();
+                    dtPer.Columns.Add("Id", typeof(int));
+                    dtPer.Columns.Add("Denumire", typeof(string));
+
+                    dtPer.Rows.Add(1, "Pana in " + (Convert.ToDateTime(txtAnLuna.Value).Year - 1).ToString() + " inclusiv");
+                    dtPer.Rows.Add(2, "Pana in " + (Convert.ToDateTime(txtAnLuna.Value).Year - 2).ToString() + " inclusiv");
+                    dtPer.Rows.Add(3, "Mai vechi");
+                }
+
+                string sql = "";
+                DataTable dt = General.IncarcaDT(sql, null);
+                grDate.KeyFieldName = "IdAuto";
+                grDate.DataSource = dt;              
+                grDate.DataBind();
+                Session["AnulareCO_Grid"] = dt;   
+
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, Path.GetFileName(Page.AppRelativeVirtualPath), new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+        private void Anulare()
+        {
+            try
+            {
+                List<int> lstMarci = new List<int>();
+                List<object> lst = grDate.GetSelectedFieldValues(new string[] { "F10003" });
+                if (lst == null || lst.Count() == 0 || lst[0] == null)
+                {
+                    pnlCtl.JSProperties["cpAlertMessage"] = Dami.TraduCuvant("Nu ati selectat niciun angajat!");
+                    return;
+                }
+
+                for (int i = 0; i < lst.Count(); i++)
+                {
+                    lstMarci.Add(Convert.ToInt32(General.Nz(lst[i], -99)));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, Path.GetFileName(Page.AppRelativeVirtualPath), new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+
+        protected void btnFiltru_Click(object sender, EventArgs e)
+        {
+            IncarcaGrid();
+        }
+
+
+    }
+
+
+}

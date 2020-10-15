fe3e63443e0629f365118ec41b0d27f910587833
Florin Ciurlau <florin.ciurlau@Wizrom.ro>
2020-10-15 16:37:02 +0300

    #614 - La logout, in cazul in care autentificarea este de tip 6 (SSO cu SAML2) ...

---
+++ WizOne/LogOut.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using WizOne.Module;
+using WizOne.Module.Saml;
+
+namespace WizOne
+{
+    public partial class LogOut : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                txtTitlu.InnerText = Dami.TraduCuvant("Ati fost delogat cu succes !");
+                lnkBack.InnerText = Dami.TraduCuvant("Inapoi in site");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex,MessageBox.icoError, "");
+                General.MemoreazaEroarea(ex, Path.GetFileName(Page.AppRelativeVirtualPath), new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+    }
+}

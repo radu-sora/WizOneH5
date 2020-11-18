e843a7d69605cecb9a55cb3b2b9410b483060d17
Florin Ciurlau <florin.ciurlau@Wizrom.ro>
2020-11-18 17:50:28 +0200

    - organigrama

--- WizOne/Module/General.cs
+++ WizOne/Module/General.cs
@@ -8948,5 +8948,177 @@ namespace WizOne.Module
 
         }
 
+        public static void SalveazaPost(object f10003, object idPost, DateTime dtModif)
+        {
+            try
+            {
+                if (f10003 == null || idPost == null) return;
+
+                string sqlPost =
+                    $@"BEGIN
+                        IF ((SELECT COUNT(*) FROM ""Org_relPostAngajat"" WHERE F10003=@1 AND {General.TruncateDate("DataInceput")} = {General.ToDataUniv(dtModif)} AND {General.ToDataUniv(dtModif)} <= {General.TruncateDate("DataSfarsit")}) <> 0)
+                            BEGIN
+                                IF (@2 <> -99)
+                                    BEGIN
+                                        IF ((SELECT COUNT(*) FROM ""Org_relPostAngajat"" WHERE F10003=@1 AND ""IdPost""=@2 AND {General.TruncateDate("DataSfarsit")}={General.ToDataUniv(dtModif.AddDays(-1))}) = 1)
+                                            BEGIN
+                                                DELETE ""Org_relPostAngajat"" WHERE F10003=@1 AND {General.TruncateDate("DataInceput")}  = {General.ToDataUniv(dtModif)} AND {General.ToDataUniv(dtModif)} <= {General.TruncateDate("DataSfarsit")};
+                                                UPDATE ""Org_relPostAngajat"" SET ""DataSfarsit""={General.ToDataUniv(2100, 1, 1)} WHERE F10003=@1 AND ""IdPost""=@2 AND {General.TruncateDate("DataSfarsit")}={General.ToDataUniv(dtModif.AddDays(-1))};
+                                            END;
+                                        ELSE
+                                            UPDATE ""Org_relPostAngajat"" SET ""IdPost"" = @2 WHERE F10003=@1 AND {General.TruncateDate("DataInceput")}  = {General.ToDataUniv(dtModif)} AND {General.ToDataUniv(dtModif)} <= {General.TruncateDate("DataSfarsit")}
+                                    END;                                
+                                ELSE
+                                    DELETE ""Org_relPostAngajat"" WHERE F10003=@1 AND {General.TruncateDate("DataInceput")}  = {General.ToDataUniv(dtModif)} AND {General.ToDataUniv(dtModif)} <= {General.TruncateDate("DataSfarsit")};
+                            END;
+                        ELSE
+                            BEGIN
+			                    DECLARE @Id int
+			                    SELECT @Id=IdAuto FROM ""Org_relPostAngajat"" WHERE  F10003=@1 AND {General.TruncateDate("DataInceput")}  <= {General.ToDataUniv(dtModif)} AND {General.ToDataUniv(dtModif)} <= {General.TruncateDate("DataSfarsit")};
+
+                                IF (@2 <> -99)
+                                    BEGIN
+                                        INSERT INTO ""Org_relPostAngajat""(""IdPost"", F10003, ""DataInceput"", ""DataSfarsit"", ""IdPostVechi"", USER_NO, TIME) VALUES(@2, @1, {General.ToDataUniv(dtModif)}, 
+                                        COALESCE((SELECT ""DataSfarsit"" FROM ""Org_relPostAngajat"" WHERE IdAuto=@Id), {General.ToDataUniv(2100, 1, 1)}), 
+                                        (SELECT ""IdPost"" FROM ""Org_relPostAngajat"" WHERE IdAuto=@Id), @3, {General.CurrentDate()});
+                                    END;                            
+                                UPDATE ""Org_relPostAngajat"" SET ""DataSfarsit""={General.ToDataUniv(dtModif.AddDays(-1))} WHERE IdAuto=@Id;
+                            END;                     
+                    END;";
+                General.ExecutaNonQuery(sqlPost, new object[] { f10003, idPost, HttpContext.Current.Session["UserId"] });
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, "SalveazaPost", new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+        public static void AdaugaBeneficiile(ref DataSet ds, object f10003, DataRow drPost)
+        {
+            try
+            {
+                string sqlFinal = "SELECT * FROM \"Admin_Beneficii\" WHERE \"Marca\" = " + f10003.ToString();
+                DataTable dtBen = new DataTable();
+                if (ds.Tables.Contains("Admin_Beneficii"))
+                {
+                    dtBen = ds.Tables["Admin_Beneficii"];
+                }
+                else
+                {
+                    dtBen = General.IncarcaDT($@"SELECT * FROM ""Admin_Beneficii"" WHERE ""Marca"" = @1", new object[] { f10003 });
+                    dtBen.TableName = "Admin_Beneficii";
+                    dtBen.PrimaryKey = new DataColumn[] { dtBen.Columns["IdAuto"] };
+                    ds.Tables.Add(dtBen);
+                }
+
+                //stergem toate beneficiile existente
+                for (int i = 0; i < dtBen.Rows.Count; i++)
+                {
+                    dtBen.Rows[i].Delete();
+                }
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    int idBen = Convert.ToInt32(General.Nz(drPost["IdBeneficiu" + i], -99));
+                    if (idBen != -99 && dtBen.Select("IdObiect=" + idBen).Count() <= 0)
+                    {
+                        DataRow drBen = ds.Tables["Admin_Beneficii"].NewRow();
+                        drBen["Marca"] = f10003;
+                        drBen["IdObiect"] = idBen;
+                        drBen["DataPrimire"] = DateTime.Now;
+                        drBen["DataExpirare"] = new DateTime(2100, 1, 1);
+                        drBen["TIME"] = DateTime.Now;
+                        drBen["USER_NO"] = HttpContext.Current.Session["UserId"] ?? DBNull.Value;
+
+                        if (Constante.tipBD == 1)
+                            drBen["IdAuto"] = Convert.ToInt32(General.Nz(dtBen.AsEnumerable().Where(p => p.RowState != DataRowState.Deleted).Max(p => p.Field<int?>("IdAuto")), 0)) + 1;
+                        else
+                            drBen["IdAuto"] = Dami.NextId("Admin_Beneficii");
+                        if (Convert.ToInt32(drBen["IdAuto"].ToString()) < 1000000)
+                            drBen["IdAuto"] = Convert.ToInt32(drBen["IdAuto"].ToString()) + 1000000;
+
+                        dtBen.Rows.Add(drBen);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, "AdaugaBeneficiile", new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+        public static void AdaugaDosar(ref DataSet ds, object f10003)
+        {
+            try
+            {
+                DataTable dtDosar = new DataTable();
+                if (ds.Tables.Contains("Admin_Dosar"))
+                {
+                    dtDosar = ds.Tables["Admin_Dosar"];
+                }
+                else
+                {
+                    dtDosar = General.IncarcaDT(@"SELECT * FROM ""Admin_Dosar"" WHERE F10003=@1", new object[] { f10003 });
+                    dtDosar.TableName = "Admin_Dosar";
+                    dtDosar.PrimaryKey = new DataColumn[] { dtDosar.Columns["F10003"], dtDosar.Columns["IdObiect"] };
+                    ds.Tables.Add(dtDosar);
+                }
+
+                //stergem inregistrarile
+                for (int i = 0; i < dtDosar.Rows.Count; i++)
+                {
+                    if (dtDosar.Rows[i].RowState != DataRowState.Deleted && General.Nz(dtDosar.Rows[i]["Obligatoriu"], 0).ToString() == "1" && dtDosar.Rows[i]["Fisier"] == DBNull.Value)
+                        dtDosar.Rows[i].Delete();
+                }
+
+                //DataTable dtOrg = General.IncarcaDT($@"SELECT * FROM ""Org_PosturiDosar"" WHERE ""IdPost""=@1 AND ""IdObiect"" NOT IN (SELECT ""IdObiect"" FROM ""Admin_Dosar"" WHERE F10003=@2)", new object[] { HttpContext.Current.Session["MP_IdPost"], f10003 });
+                DataTable dtOrg = General.IncarcaDT($@"SELECT * FROM ""Org_PosturiDosar"" WHERE ""IdPost""=@1", new object[] { HttpContext.Current.Session["MP_IdPost"] });
+
+                for (int i = 0; i < dtOrg.Rows.Count; i++)
+                {
+                    if (dtDosar.Select("IdObiect=" + dtOrg.Rows[i]["IdObiect"]).Count() == 0)
+                    {
+                        DataRow drDsr = dtDosar.NewRow();
+                        drDsr["F10003"] = f10003;
+                        drDsr["IdObiect"] = dtOrg.Rows[i]["IdObiect"];
+                        drDsr["AreFisier"] = 0;
+                        drDsr["Obligatoriu"] = 1;
+                        drDsr["TIME"] = DateTime.Now;
+                        drDsr["USER_NO"] = HttpContext.Current.Session["UserId"] ?? DBNull.Value;
+
+                        //if (Constante.tipBD == 1)
+                        //    drDsr["IdAuto"] = Convert.ToInt32(General.Nz(dtDosar.AsEnumerable().Where(p => p.RowState != DataRowState.Deleted).Max(p => p.Field<int?>("IdAuto")), 0)) + 1;
+                        //else
+                        //    drDsr["IdAuto"] = Dami.NextId("Admin_Dosar");
+                        //if (Convert.ToInt32(drDsr["IdAuto"].ToString()) < 1000000)
+                        //    drDsr["IdAuto"] = Convert.ToInt32(drDsr["IdAuto"].ToString()) + 1000000;
+
+                        dtDosar.Rows.Add(drDsr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, "AdaugaDosar", new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+        public static void AflaIdPost()
+        {
+            try
+            {
+                if (HttpContext.Current.Session["MP_IdPost"] == null)
+                {
+                    string sqlIdPost = $@"SELECT ""IdPost"" FROM ""Org_relPostAngajat"" WHERE F10003=@1 AND {General.TruncateDate("DataInceput")} <= {General.CurrentDate(true)} AND {General.CurrentDate(true)} <= {General.TruncateDate("DataSfarsit")}";
+                    HttpContext.Current.Session["MP_IdPost"] = General.ExecutaScalar(sqlIdPost, new object[] { HttpContext.Current.Session["Marca"] });
+                }
+            }
+            catch (Exception ex)
+            {
+                General.MemoreazaEroarea(ex, "AdaugaDosar", new StackTrace().GetFrame(0).GetMethod().Name);
+            }
+        }
+
+
     }
 }

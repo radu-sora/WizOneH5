b3391ae1a0429321b368aacfc8cc16910f45d03d
radu.sora <radu.sora@WizromSoftwareSRL.onmicrosoft.com>
2021-05-11 11:15:59 +0300

    #926 - Avs/Cereri

--- WizOne/Avs/Cereri.aspx.cs
+++ WizOne/Avs/Cereri.aspx.cs
@@ -5273,69 +5273,19 @@ namespace WizOne.Avs
                         General.ExecutaNonQuery(sqlCtr);
                         break;
                     case (int)Constante.Atribute.TipContract:
-                    case (int)Constante.Atribute.DurataContract:
-                        {
-                            int nrLuni = 0, nrZile = 0;
-                            string sql100Tmp = "";
-                            DateTime dtTmp = new DateTime();
-                            DateTime dtf = new DateTime(2100, 1, 1, 0, 0, 0);
-                            if (dtSf != dtf)
-                                dtTmp = dtSf.AddDays(1);
-                            else
-                                dtTmp = dtSf;
-
-                            bool modifTip = false;
-                            bool modifDur = false;
-
-                            Personal.Contract ctr = new Personal.Contract();
-                            ctr.CalculLuniSiZile(Convert.ToDateTime(dtInc.Date), Convert.ToDateTime(dtSf.Date), out nrLuni, out nrZile);
-
-                            if (dtF100.Rows[0]["F100984"].ToString() != dtCer.Rows[0]["TipContract"].ToString())
-                                modifTip = true;
-
-                            if ((dtF100.Rows[0]["DurataContract"] != null && dtF100.Rows[0]["F1009741"].ToString() != dtCer.Rows[0]["DurataContract"].ToString())
-                                || (dtF100.Rows[0]["DataInceputCIM"] != null &&  dtF100.Rows[0]["F100933"].ToString() != dtCer.Rows[0]["DataInceputCIM"].ToString())
-                                || (dtF100.Rows[0]["DataSfarsitCIM"] != null && dtF100.Rows[0]["F100934"].ToString() != dtCer.Rows[0]["DataSfarsitCIM"].ToString()))
-                                modifDur = true;
-
+                        {   
                             if (dtModif.Year == dtLucru.Year && dtModif.Month == dtLucru.Month && dtF100 != null && dtF100.Rows.Count > 0)
                             {                               
-                                sql100Tmp = "UPDATE F100 SET " + (modifTip ? "F100984 = " + dtCer.Rows[0]["TipContract"].ToString() : "") + (modifDur ? (modifTip ? "," : "") +
-                                    (dtCer.Rows[0]["DurataContract"].ToString() == "1" ? " F10023 = " + General.ToDataUniv(new DateTime(2100, 1, 1))
-                                    + ", F100993 = " + General.ToDataUniv(new DateTime(2100, 1, 1)) + ", F1009741 = 1, F100935 = 0, F100936 = 0 "
-                                    : "F100933 = " + data9 + ", F100934 = " + data10 + ", F100936 = " + nrZile.ToString() + ", F100935 = " + nrLuni.ToString() + ", F100938 = 1, F10023 = " + data10
-                                    + ", F100993 = " + (Constante.tipBD == 1 ? "CONVERT(DATETIME, '" + dtTmp.Day.ToString().PadLeft(2, '0') + "/" + dtTmp.Month.ToString().PadLeft(2, '0') + "/" + dtTmp.Year.ToString() + "', 103)"
-                                    : "TO_DATE('" + dtTmp.Day.ToString().PadLeft(2, '0') + "/" + dtTmp.Month.ToString().PadLeft(2, '0') + "/" + dtTmp.Year.ToString() + "', 'dd/mm/yyyy')") + ", F1009741 = " 
-                                    + (dtCer.Rows[0]["DurataContract"] == DBNull.Value || dtCer.Rows[0]["DurataContract"].ToString().Length <= 0 ? "NULL" : dtCer.Rows[0]["DurataContract"].ToString()) )
-                                    : "") +  " WHERE F10003 = " + f10003.ToString();
+                                sql100 = "UPDATE F100 SET F100984 = " + dtCer.Rows[0]["TipContract"].ToString() + " WHERE F10003 = " + f10003.ToString();
 
-                                sql1001 = "UPDATE F1001 SET " + (modifTip ? "F1001137 = " + data : "") + (modifDur ? (modifTip ? "," : "") + "F1001138 = " + data : "") + " WHERE F10003 = " + f10003.ToString();
-
-
-                                General.IncarcaDT(sql100Tmp, null); //pentru ca se modifica F10023, trebuie rulat CalculCO, de aceea se executa aici si nu la sfarsit
+                                sql1001 = "UPDATE F1001 SET F1001137 = " + data  + " WHERE F10003 = " + f10003.ToString();                                                               
                             }
                             else
                                 sql1001 = "UPDATE F1001 SET F1001137 = " + data + " WHERE F10003 = " + f10003.ToString();
 
-                            if (modifTip)
-                                sql = "INSERT INTO F704 (F70401, F70402, F70403, F70404, F70405, F70406, F70407, F70409, F70410, F70420, F70468, F70469, USER_NO, TIME) "
-                                    + " VALUES (704, " + idComp.ToString() + ", " + f10003.ToString() + ", 35, 'Tip contract', " + data + ", " + dtCer.Rows[0]["TipContract"].ToString() + ", 'Modificari in avans', '"
-                                    + dateDoc + "', " + act.ToString() + ", " + General.ToDataUniv(new DateTime(1900, 1, 1)) + ", " + General.ToDataUniv(new DateTime(1900, 1, 1)) + ", -9, " + (Constante.tipBD == 1 ? "getdate()" : "sysdate") + ")";
-
-                            if (modifDur)
-                                sql1 = "INSERT INTO F704 (F70401, F70402, F70403, F70404, F70405, F70406, F70407, F70468, F70469, F70409, F70410, F70420, USER_NO, TIME) "
-                                    + " VALUES (704, " + idComp.ToString() + ", " + f10003.ToString() + ", 36, 'Durata contract', " + data + ", " + dtCer.Rows[0]["DurataContract"].ToString() + "," +
-                                    (dtCer.Rows[0]["DurataContract"].ToString() == "1" ? General.ToDataUniv(new DateTime(2100, 1, 1)) + ", " + General.ToDataUniv(new DateTime(2100, 1, 1)) + ", " : data9 + ", " + data10 + ", ") 
-                                    + " 'Modificari in avans', '" + dateDoc + "', " + act.ToString() + ", -9, " + (Constante.tipBD == 1 ? "getdate()" : "sysdate") + ")";
-
-                            //Radu 01.11.2019 - se modifica data plecarii, deci trebuie refacut CalculCO   
-                            if (dtSf.Year > DateTime.Now.Year)
-                            {
-                                General.CalculCO(dtSf.Year, f10003);
-                                General.CalculCO(DateTime.Now.Year, f10003);
-                            }
-                            else
-                                General.CalculCO(dtSf.Year, f10003);
+                            sql = "INSERT INTO F704 (F70401, F70402, F70403, F70404, F70405, F70406, F70407, F70409, F70410, F70420, F70468, F70469, USER_NO, TIME) "
+                                + " VALUES (704, " + idComp.ToString() + ", " + f10003.ToString() + ", 35, 'Tip contract', " + data + ", " + dtCer.Rows[0]["TipContract"].ToString() + ", 'Modificari in avans', '"
+                                + dateDoc + "', " + act.ToString() + ", " + General.ToDataUniv(new DateTime(1900, 1, 1)) + ", " + General.ToDataUniv(new DateTime(1900, 1, 1)) + ", -9, " + (Constante.tipBD == 1 ? "getdate()" : "sysdate") + ")";                                  
                         }
                         break;
                     case (int)Constante.Atribute.Post:

3f7c87e9e487574a729d396f23f935b4c88a31af
Florin Ciurlau <florin.ciurlau@Wizrom.ro>
2021-06-08 13:43:14 +0300

    - validare valoare idFunctie

--- WizOne/Avs/Cereri.aspx.cs
+++ WizOne/Avs/Cereri.aspx.cs
@@ -4671,24 +4671,25 @@ namespace WizOne.Avs
                         break;
                     case (int)Constante.Atribute.Functie:
                         {
+                            string idFunctie = General.Nz(dtCer.Rows[0]["FunctieId"], -99).ToString();
                             if (dtModif.Year == dtLucru.Year && dtModif.Month == dtLucru.Month && dtF100 != null && dtF100.Rows.Count > 0)
                             {
                                 //Florin 2021.03.02  #710 - am adaugat selecturile la zile proba ZL, zile proba ZC, zile demisie si zile concediere
                                 act = 1;
-                                sql100 = "UPDATE F100 SET F10071 = " + dtCer.Rows[0]["FunctieId"].ToString() + ", F100992 = " + data +
-                                    ", F100975 = (SELECT NrZileLucrProba FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + "))" +
-                                    ", F1009742 = (SELECT NrZileDemisie FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + "))" +
-                                    ", F100931 = (SELECT NrZileConcediere FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + "))" + 
+                                sql100 = "UPDATE F100 SET F10071 = " + idFunctie + ", F100992 = " + data +
+                                    ", F100975 = (SELECT NrZileLucrProba FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + idFunctie + "))" +
+                                    ", F1009742 = (SELECT NrZileDemisie FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + idFunctie + "))" +
+                                    ", F100931 = (SELECT NrZileConcediere FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + idFunctie + "))" + 
                                     " WHERE F10003 = " + f10003.ToString();
                                 if (dtF1001 != null && dtF1001.Rows.Count > 0)
-                                    sql1001 = "UPDATE F1001 SET F1001063 = (SELECT NrZileCalProba FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + ")) WHERE F10003 = " + f10003.ToString();
+                                    sql1001 = "UPDATE F1001 SET F1001063 = (SELECT NrZileCalProba FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + idFunctie + ")) WHERE F10003 = " + f10003.ToString();
 
                                 //Florin 2021.03.11 - am mutat functia la Post
                                 ////Florin 2020.10.06
                                 //General.SalveazaPost(f10003, dtCer.Rows[0]["PostId"], Convert.ToDateTime(dtCer.Rows[0]["DataModif"]));
                             }
                             sql = "INSERT INTO F704 (F70401, F70402, F70403, F70404, F70405, F70406, F70407, F70409, F70410, F70420, USER_NO, TIME) "
-                            + " VALUES (704, " + idComp.ToString() + ", " + f10003.ToString() + ", 2, 'Functie', " + data + ", " + dtCer.Rows[0]["FunctieId"].ToString() + ", 'Modificari in avans', '"
+                            + " VALUES (704, " + idComp.ToString() + ", " + f10003.ToString() + ", 2, 'Functie', " + data + ", " + idFunctie + ", 'Modificari in avans', '"
                             + dateDoc + "', " + act.ToString() + ", -9, " + (Constante.tipBD == 1 ? "getdate()" : "sysdate") + ")";
                         }
                         break;
@@ -4710,10 +4711,11 @@ namespace WizOne.Avs
                                     //    sql1001 = "UPDATE F1001 SET F100956 = " + data + ", F1001063 = " + dtCer.Rows[0]["PerProbaZC"].ToString() + " WHERE F10003 = " + f10003.ToString();
 
                                     //Florin 2021.03.02  #710 - am adaugat selecturile la zile proba ZL, zile proba ZC, zile demisie si zile concediere
+                                    string idFunctie = General.Nz(dtCer.Rows[0]["FunctieId"], -99).ToString();
                                     sql100 = "UPDATE F100 SET F10098 = " + dtCer.Rows[0]["CORCod"].ToString() +
-                                            ", F100975 = (SELECT NrZileLucrProba FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + "))" +
-                                            ", F1009742 = (SELECT NrZileDemisie FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + "))" +
-                                            ", F100931 = (SELECT NrZileConcediere FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + "))" +
+                                            ", F100975 = (SELECT NrZileLucrProba FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + idFunctie + "))" +
+                                            ", F1009742 = (SELECT NrZileDemisie FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + idFunctie + "))" +
+                                            ", F100931 = (SELECT NrZileConcediere FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + idFunctie + "))" +
                                             " WHERE F10003 = " + f10003.ToString();
                                     if (dtF1001 != null && dtF1001.Rows.Count > 0)
                                         sql1001 = "UPDATE F1001 SET F100956 = " + data + ", F1001063 = (SELECT NrZileCalProba FROM tblNivelFunctie WHERE Id = (SELECT F71813 FROM F718 WHERE F71802=" + dtCer.Rows[0]["FunctieId"].ToString() + ")) WHERE F10003 = " + f10003.ToString();
